e98124b788a6340f0ea4af75ceb02713283f3318
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2024-11-02 09:53:39 -0500

    read me and remaining reducer added

--- src/context.js
+++ src/context.js
@@ -1,9 +1,9 @@
-import React, {useState, useContext, useReducer, useEffect} from 'react;'
+import React, { useContext, useReducer, useEffect} from 'react';
 import cartItems from'./data';
 import reducer from './reducer'
-import { type } from '@testing-library/user-event/dist/type';
 
-const url = 'https://course-api.com/react-useReducer-cart-project'
+
+const url = 'https://www.course-api.com/react-useReducer-cart-project'
 const AppContext = React.createContext()
 
 const initialState = {
